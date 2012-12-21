a36888b177e3fef2dcc2980ff0a4da94aea3b928
23 Cats Server <metapyziks@gmail.com>
2012-12-21 18:27:59 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
-OLDDIR=pwd
+OLDDIR="$(pwd)"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Entering $DIR"
 cd $DIR
 
-MONO=pidof mono
-if [ -n $MONO ] ; then
+MONO="$(pidof mono)"
+if [ $MONO ]
+then
 	echo "Stopping active server with pid ${MONO}"
 	kill $MONO
+else
+	echo "Server not currently running, so will not be restarted"
 fi
 
 echo "Pulling new changes"
 git pull origin live
 echo "Making"
 make release
-echo "Starting server"
-sh init.sh
+
+if [ $MONO ]
+then
+	echo "Starting server"
+	sh init.sh
+fi
 
 echo "Entering $OLDDIR"
 cd $OLDDIR

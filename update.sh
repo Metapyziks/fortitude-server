2f4b42398692c89e0940e565da458d3b2ecc9bfb
James King <metapyziks@gmail.com>
2012-12-21 18:09:22 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,8 +6,17 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Entering $DIR"
 cd $DIR
 
+MONO=pidof mono
+if [ -n $MONO ] ; then
+	echo "Stopping active server with pid ${MONO}"
+	kill $MONO
+fi
+
+echo "Pulling new changes"
 git pull origin master
+echo "Making"
 make release
+echo "Starting server"
 sh init.sh
 
 echo "Entering $OLDDIR"

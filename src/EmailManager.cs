9177df4a6812b2e9748a758397585aed69691d3c
James King <metapyziks@gmail.com>
2013-01-20 23:04:22 +0000

    Added ssl option to config.ini

--- src/EmailManager.cs
+++ src/EmailManager.cs
@@ -19,31 +19,31 @@ namespace TestServer
             get { return stClient != null; }
         }
 
-        public static void CreateClient( IniSection settings )
+        public static void CreateClient(IniSection settings)
         {
             CreateClient( settings.GetValue( "server" ),
                 Int32.Parse( settings.GetValue( "port" ) ),
                 settings.GetValue( "address" ),
                 settings.GetValue( "username" ),
-                settings.GetValue( "password" ) );
+                settings.GetValue( "password" ),
+                Boolean.Parse( settings.GetValue("ssl")));
         }
 
-        public static void CreateClient( String smtpAddress, int port, String email, String username, String password )
+        public static void CreateClient(String smtpAddress, int port, String email, String username, String password, bool ssl)
         {
             stAdminEmail = email;
 
-            stClient = new SmtpClient( smtpAddress, port )
-            {
-                Credentials = new NetworkCredential( username, password ),
-                EnableSsl = true
+            stClient = new SmtpClient(smtpAddress, port) {
+                Credentials = new NetworkCredential(username, password),
+                EnableSsl = ssl
             };
             ServicePointManager.ServerCertificateValidationCallback =
-                ( s, certificate, chain, sslPolicyErrors ) => true;
+                (s, certificate, chain, sslPolicyErrors) => true;
         }
 
-        public static void Send( String to, String subject, String message )
+        public static void Send(String to, String subject, String message)
         {
-            stClient.Send( stAdminEmail, to, subject, message );
+            stClient.Send(stAdminEmail, to, subject, message);
         }
     }
 }

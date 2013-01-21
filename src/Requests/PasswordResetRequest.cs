7be6b8c3a991e97024d1fa5f76efa530548e2bef
James King <metapyziks@gmail.com>
2013-01-21 23:05:59 +0000

    Added sendpassreset API command

---
+++ src/Requests/PasswordResetRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestServer.Requests;
+
+namespace TestServer.src.Requests
+{
+    [RequestTypeName("sendpassreset")]
+    class PasswordResetRequest : Request
+    {
+        public override Responses.Response Respond(NameValueCollection args)
+        {
+            EmailValidationCode valid;
+            return EmailValidationCode.AttemptCreate(EmailValidationType.ResetPassword, args["email"], out valid)
+                ?? new Responses.Response(true);
+        }
+    }
+}

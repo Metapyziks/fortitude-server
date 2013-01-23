e137a5b470808ec50319229d13100c49b74477e7
James King <metapyziks@gmail.com>
2013-01-23 18:02:44 +0000

    Emails are now sent asynchronously and so shouldn't cause any timeouts

--- src/EmailManager.cs
+++ src/EmailManager.cs
@@ -43,7 +43,7 @@ namespace TestServer
 
         public static void Send(String to, String subject, String message)
         {
-            stClient.Send(stAdminEmail, to, subject, message);
+            stClient.SendMailAsync(stAdminEmail, to, subject, message);
         }
     }
 }

b334b6e59d47fee9a7d544a55a910be1d7b78da2
James King <metapyziks@gmail.com>
2013-01-27 02:35:00 +0000

    Can now use "uid" or "uids" instead of "uname" or "unames" in a /api/userinfo request

--- src/Requests/UserInfoRequest.cs
+++ src/Requests/UserInfoRequest.cs
@@ -16,18 +16,33 @@ namespace TestServer.Requests
         public override Responses.Response Respond(NameValueCollection args)
         {
             String[] usernames = null;
+            int[] userids = null;
 
-            if (args["uname"] != null)
+            if (args["uname"] != null) {
                 usernames = new String[] { args["uname"] };
-            else if (args["unames"] != null)
+            } else if (args["uid"] != null) {
+                userids = new int[] { Int32.Parse(args["uid"]) };
+            } else if (args["unames"] != null) {
                 usernames = args["unames"].Split(new char[] { ',' },
                     StringSplitOptions.RemoveEmptyEntries);
+            } else if (args["uids"] != null) {
+                userids = args["uids"].Split(new char[] { ',' },
+                    StringSplitOptions.RemoveEmptyEntries).Select(x => Int32.Parse(x)).ToArray();
+            }
 
-            if (usernames == null || usernames.Length == 0)
+            if ((usernames == null || usernames.Length == 0) && (userids == null || userids.Length == 0)) {
                 return new Responses.ErrorResponse("no username given");
+            }
 
-            List<Account> users = DatabaseManager.Select(usernames.Select(
-                x => (AccountPred) (acc => acc.Username == x)).ToArray());
+            List<Account> users;
+
+            if (usernames != null) {
+                users = DatabaseManager.Select(usernames.Select(
+                    x => (AccountPred) (acc => acc.Username == x)).ToArray());
+            } else {
+                users = DatabaseManager.Select(userids.Select(
+                    x => (AccountPred) (acc => acc.AccountID == x)).ToArray());
+            }
 
             return new Responses.UserInfoResponse(users);
         }

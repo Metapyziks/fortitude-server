df47996e57962100fbeb0e6417ca4affb80884e6
James King <metapyziks@gmail.com>
2013-02-02 00:05:57 +0000

    Added test for Tools.GetDistance

---
+++ src/CacheTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TestServer;
+
+namespace TestServerTests
+{
+    [TestClass]
+    public class CacheTests
+    {
+        [TestMethod]
+        public void CacheDistanceTest()
+        {
+            double latA = 54.774503, lngA = -1.575948;
+            double latB = 51.502972, lngB = 0.003101;
+            double targ = 378700.0;
+            double dist = Tools.GetDistance(latA, lngA, latB, lngB);
+
+            double error = 0.05;
+
+            Assert.IsTrue(dist <= targ + targ * error,
+                String.Format("Distance is too large! Expected {0}, got {1}.", targ, dist));
+            Assert.IsTrue(dist >= targ - targ * error,
+                String.Format("Distance is too small! Expected {0}, got {1}.", targ, dist));
+        }
+    }
+}

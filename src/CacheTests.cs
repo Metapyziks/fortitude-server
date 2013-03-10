1f75e625ac3b624f8d902f025386ce11bf66e227
James King <metapyziks@gmail.com>
2013-03-10 16:16:45 +0000

    Added CacheTests

--- src/CacheTests.cs
+++ src/CacheTests.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using FortitudeServer;
+using FortitudeServer.Entities;
 
 namespace TestServerTests
 {
@@ -32,7 +34,22 @@ namespace TestServerTests
         [TestMethod]
         public void GrowthStyles()
         {
+            GrowthStyle[] styles = new[] {
+                GrowthStyle.Constant,
+                GrowthStyle.Logarithmic,
+                GrowthStyle.Linear,
+                GrowthStyle.Quadratic };
 
+            int initial = 50;
+
+            foreach (var style in styles) {
+                Debug.WriteLine("Testing growth of {0}:", style);
+                for (int i = 0; i < 10; ++i) {
+                    Debug.WriteLine("- attack #{0} ", NonPlayerCache.FindNextGarrisonSize(initial, i, style));
+                }
+            }
+
+            Assert.IsTrue(true);
         }
     }
 }

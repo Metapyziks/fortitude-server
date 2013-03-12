49b2e47dedfef0a9c04468d061818bdfcd15a211
Aethelar <e.c.nugee@hotmail.co.uk>
2013-03-12 11:40:37 +0000

    website update

--- res/parts/account_script.js
+++ res/parts/account_script.js
@@ -8,3 +8,20 @@ function updateAvatar() {
 	new_avatar.innerHTML = '<img src="/images/avatars/' + avatar + '-normal.gif" class="avatar" />';
 }
 
+function delete_account (account_id, email) {
+	alert ('(dummy function)' + account_id + ' ' + email);
+	document.getElementById("confirm_delete").innerHTML = "An email has been sent to you containing a link to delete your account.";
+}
+
+function updatePasswords(form) {
+	var valid = validatePassword(form.new_pword.value, form.new_rpword.value);
+	if (valid) {
+		form.pword_phash.value = hash(form.pword.value);
+		form.new_pword_phash.value = hash(form.new_pword.value);
+	}
+
+	form.new_pword.value = "";
+	form.new_rpword.value = "";
+
+	return valid;
+}

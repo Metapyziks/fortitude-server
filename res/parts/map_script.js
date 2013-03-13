a23bad1397e3e537ef94a320ee14c9189f00e498
Aethelar <e.c.nugee@hotmail.co.uk>
2013-03-13 17:28:28 +0000

    map on website

--- res/parts/map_script.js
+++ res/parts/map_script.js
@@ -1,24 +1,57 @@
-function initialize() {
-			var mapOptions = {
-			  center: new google.maps.LatLng(54.778599, -1.570315),
-			  zoom: 14,
-			  mapTypeId: google.maps.MapTypeId.TERRAIN
-			};
-			var map = new google.maps.Map(document.getElementById("map_canvas"),
-			  mapOptions);
-			var mapCaches = new Array();
-	<?
-	  foreach (var cache in allCaches) {
-	?>
-			mapCaches.push(new google.maps.Marker({
-			  position: new google.maps.LatLng({$cache.Latitude},{$cache.Longitude}),
+	
+	var mapCaches = new Array();
+
+	var map;
+
+	function initialize() 
+	{
+		var mapOptions = {
+		  	center: new google.maps.LatLng(54.778599, -1.570315),
+			zoom: 14,
+			mapTypeId: google.maps.MapTypeId.TERRAIN
+		};
+		
+		map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+
+		return map;
+	}
+
+	function addMarker (latitude, longitude, name, map) 
+	{
+		mapCaches.push(new google.maps.Marker({
+			  position: new google.maps.LatLng(latitude,longitude),
 			  map: map,
-			  title:"{$cache.Name}"
+			  title: name
 			}));
-	<?
-	  }
-	  // End for each var cache in cacheas
-	?>
-		  }
-		  
-		  window.onload = initialize();
+	}
+
+	function clearMap ()
+	{
+		while(mapCaches.length != 0)
+		{
+			mapCaches.pop().remove();
+		}
+	}
+
+	function processCoords(loc)
+	{
+		map.setCenter (loc);
+	}
+
+	function get_coords(address)
+	{
+	    var gc      = new google.maps.Geocoder(),
+	        opts    = { 'address' : address };
+
+	    gc.geocode(opts, function (results, status)
+	    {
+	        if (status == google.maps.GeocoderStatus.OK)
+	        {   
+	            processCoords(results[0].geometry.location);
+	        }
+	        else
+	        {
+	            document.getElementById("page_response").innerHTML = "<ul class='news'><li><p>We could not find that location. Please try again.</p></li></ul>";
+	        }
+	    });
+	}

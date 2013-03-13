c2994878aeaaa74ae6fb32ee5054c9054a2c70b0
Aethelar <e.c.nugee@hotmail.co.uk>
2013-03-13 22:26:04 +0000

    yet more website

--- res/parts/map_script.js
+++ res/parts/map_script.js
@@ -1,7 +1,10 @@
-	
-	var mapCaches = new Array();
+	var alliedCacheIcon = 'http://www.google.com/intl/en_us/mapfiles/ms/micons/blue.png';
+	var enemyCacheIcon = 'http://www.google.com/intl/en_us/mapfiles/ms/micons/red.png';
+	var unownedCacheIcon = 'http://www.google.com/intl/en_us/mapfiles/ms/micons/yellow.png';
+	var adminCacheIcon = 'http://www.google.com/intl/en_us/mapfiles/ms/micons/green.png';	
 
-	var map;
+	var mapCaches = new Array();
+        var map;
 
 	function initialize() 
 	{
@@ -12,17 +15,6 @@
 		};
 		
 		map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-
-		return map;
-	}
-
-	function addMarker (latitude, longitude, name, map) 
-	{
-		mapCaches.push(new google.maps.Marker({
-			  position: new google.maps.LatLng(latitude,longitude),
-			  map: map,
-			  title: name
-			}));
 	}
 
 	function clearMap ()
@@ -54,4 +46,44 @@
 	            document.getElementById("page_response").innerHTML = "<ul class='news'><li><p>We could not find that location. Please try again.</p></li></ul>";
 	        }
 	    });
+	}
+
+	function addAlliedMarker (latitude, longitude, name) 
+	{
+		mapCaches.push(new google.maps.Marker({
+			  position: new google.maps.LatLng(latitude,longitude),
+			  map: map,
+			  title: name,
+			  icon: alliedCacheIcon
+			}));
+	}
+
+        function addEnemyMarker (latitude, longitude, name) 
+	{
+		mapCaches.push(new google.maps.Marker({
+			  position: new google.maps.LatLng(latitude,longitude),
+			  map: map,
+			  title: name,
+			  icon: enemyCacheIcon
+			}));
+	}
+
+        function addUnownedMarker (latitude, longitude, name) 
+	{
+		mapCaches.push(new google.maps.Marker({
+			  position: new google.maps.LatLng(latitude,longitude),
+			  map: map,
+			  title: name,
+			  icon: unownedCacheIcon
+			}));
+	}
+
+        function addAdminMarker (latitude, longitude, name) 
+	{
+		mapCaches.push(new google.maps.Marker({
+			  position: new google.maps.LatLng(latitude,longitude),
+			  map: map,
+			  title: name,
+			  icon: adminCacheIcon
+			}));
 	}

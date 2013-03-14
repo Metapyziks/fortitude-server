15a8ce83db952cd8c806ead02a9ff4989b52ac58
James King <metapyziks@gmail.com>
2013-03-14 00:26:08 +0000

    Deleting caches from admin menu: complete

--- res/parts/messages_script.js
+++ res/parts/messages_script.js
@@ -4,18 +4,6 @@ if (typeof String.prototype.endsWith !== 'function') {
     };
 }
 
-function getCookie(c_name) {
-    var i,x,y,ARRcookies = document.cookie.split(";").join(",").split(",");
-    for (i = 0; i < ARRcookies.length; i++) {
-        x = ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
-        y = ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
-        x = x.replace(/^\s+|\s+$/g,"");
-        if ( x == c_name ) {
-            return unescape(y);
-        }
-    }
-}
-
 function colourMessages() {
     var _colours = ["#3c3c3c", "#303030"];
 
